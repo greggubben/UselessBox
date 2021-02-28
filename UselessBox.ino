25239c54960396f897a3732c57397883edea768d
Gregg <greggubben@gmail.com>
2021-02-28 13:26:17 -0500

    added wake up to tease

--- UselessBox.ino
+++ UselessBox.ino
@@ -1,25 +1,45 @@
+/*
+ * This code is adapted from grafalex
+ * https://www.thingiverse.com/thing:1258082
+ * 
+ * Box printed from designs above and modified by Xrew
+ * https://www.thingiverse.com/thing:2847024
+ * 
+ * Adapted the code and design for a LOLIN/WEMOS D1 Mini Pro
+ * so I can take advantage of the LiPo Battery Manager and
+ * the built in I2C connector. No soldering to the board required.
+ * 
+ * 
+ */
+
 #include <Servo.h> 
 
 // Define the IO Pins that will be used
-const uint8_t switchPin = 4; // GPIO4 aka SDA
-const uint8_t servoPin = 5;  // GPIO5 aka SCL
+// I used the 4 pin connector provided on the LOLIN D1 Mini Pro:
+const uint8_t SwitchPin = 4; // GPIO4 aka SDA
+const uint8_t ServoPin = 5;  // GPIO5 aka SCL
 
 // Define the Servo parameters
-Servo myservo;
+Servo turnSwitchOffServo;
 
 // Servo limits
-const int servoMinPos = 10;
-const int servoMaxPos = 138;
-const int slamDistance = 20;
+const int ServoMinPos = 10;
+const int ServoMaxPos = 138;
+const int SlamDistance = 20;
 
 // Range to use when in teasing mode
-const int teaseMinPos = 90;
-const int teaseMaxPos = 120;
+const int TeaseMinPos = 90;
+const int TeaseMaxPos = 120;
 
-// Click constants
-const int fastClickThreshold = 2000; //2 seconds
-const int clicksForTeaseMode = 4;
-const int teaseThreshold = 4000; // 5 seconds after the last click
+// Tease constants
+const int FastClickThreshold = 2000; //2 seconds
+const int ClicksForTeaseMode = 4;
+const int TeaseThreshold = 4000; // 5 seconds after the last click
+
+// Wakeup constants
+const unsigned long WakeupMinMillis = 40 * 60 * 1000;   // Wake up Min 40 minutes
+const unsigned long WakeupMaxMillis = 55 * 60 * 1000;   // Wake up Max 55 minutes
+unsigned long wakeup_time = WakeupMinMillis;            // Start with the Min
 
 
 // Click Status
@@ -33,10 +53,11 @@ void setup()
 { 
   Serial.begin(115200);
 
-  myservo.attach(servoPin);
-  myservo.write(servoMinPos);
+//  turnSwitchOffServo.attach(ServoPin, ServoMinPos, ServoMaxPos);
+  turnSwitchOffServo.attach(ServoPin);
+  turnSwitchOffServo.write(ServoMinPos);
   
-  pinMode(switchPin, INPUT);
+  pinMode(SwitchPin, INPUT);
 
   randomSeed(analogRead(A0));
 
@@ -47,50 +68,60 @@ void setup()
   Serial.println();
 } 
 
-bool move_servo(int end_pos, int step_delay, bool look_for_switch)
-{
-  int cur_pos = myservo.read();
-  
-  Serial.print("    Move to ");
-  Serial.print(end_pos);
-  Serial.print(" from ");
-  Serial.print(cur_pos);
-  Serial.print(" with delay ");
-  Serial.print(step_delay);
-  Serial.print(" looking for switch ");
-  Serial.println(look_for_switch);
 
-  // Check if we are already there
-  if(cur_pos == end_pos)
-  {
-    Serial.println("    Move Already there");
+void loop() 
+{
+  // Get a new random speed range each loop for variability
+  int speedTurnOff = random(1, 3);
+  int speedReturn = random(2, 8);
+
+  if(should_tease()) {
+    tease();
+  }
+
+  //TODO Sleep while not turned on
+  // Wait for click
+  if(digitalRead(SwitchPin)) {
+    
+    Serial.println("Switch turned ON");
+    
+    check_for_bothering();
+  
+    // Move forward until switch is off
+    turnOffSwitch(speedTurnOff);
+    Serial.println("Switch turned OFF");
+    
+    // Now move backward until min pos or user clicked switch again
+    servoHome(speedReturn, true);
+    Serial.println("Switch action END");
+    Serial.println();
+  }
+} 
+
+
+/**************************
+ * Tease Functions
+ **************************/
+
+bool should_tease()
+{
+
+  unsigned long cur_timestamp = millis();
+
+  if (cur_timestamp - last_click_timestamp > wakeup_time) {
+    Serial.println("  Wake Up!");
+    wakeup_time = random(WakeupMinMillis, WakeupMaxMillis);
+//    tease_mode = true;
+    last_click_timestamp = cur_timestamp;
     return true;
   }
 
-  // Move from current position to the end position  
-  int servo_step = cur_pos < end_pos ? 1 : -1;
-  int switching_state = (cur_pos >= end_pos);
-  Serial.print("    Move step ");
-  Serial.print(servo_step);
-  Serial.print(" with switching state ");
-  Serial.println(switching_state);
-  for(; cur_pos != end_pos; cur_pos += servo_step)
-  {                                
-    myservo.write(cur_pos);
-    delay(step_delay);
+  // Check whether there was no action during last few seconds so we may tease
+  if(tease_mode && (cur_timestamp - last_click_timestamp > TeaseThreshold)) {
+    return true;
+  }
 
-    // If servo is moving forward - expect for switch off
-    // if moving backward - switch on will break the loop
-    if(look_for_switch && (digitalRead(switchPin) == switching_state))
-    {
-      Serial.print("    Move Switch change detected to ");
-      Serial.println(digitalRead(switchPin));
-      return false;
-    }
-  } 
-  
-  Serial.println("    Move Done");
-  return true;
+  return false;
 }
 
 void tease()
@@ -98,36 +129,40 @@ void tease()
 
   Serial.println("  Teasing!");
   
-  move_servo(teaseMinPos, random(4, 6), false);
+  move_servo(TeaseMinPos, random(4, 6), false);
   delay(200 + random(2000));
-  
-  do
-  {
-    Serial.println("  Tease loop!");
 
-    if(digitalRead(switchPin)) // User again clicked the switch
+  int taunts = random(2,6);
+  Serial.print("Taunts ");
+  Serial.println(taunts);
+  for (int t=0; t<taunts; t++) {
+    Serial.print("  Tease loop! ");
+    Serial.println(t);
+
+    if(digitalRead(SwitchPin)) // User again clicked the switch
       return;
     
-    move_servo(teaseMaxPos, random(2, 6), false);
+    move_servo(TeaseMaxPos, random(2, 6), false);
     delay(random(1000)); 
-    move_servo(teaseMinPos, random(4, 6), false);
+    move_servo(TeaseMinPos, random(4, 6), false);
     delay(200 + random(2000));  
   }
-  while(random(5) > 2);
 
-  if(digitalRead(switchPin)) // User again clicked the switch
+  if(digitalRead(SwitchPin)) // User again clicked the switch
     return;
   
   Serial.println("  Tease Exiting loop!");
-  move_servo(servoMinPos, random(4, 6), false);
+  servoHome(random(4, 6));
   
   // No longer tease
-  if(random(5) > 2)
-  {
-    Serial.println("  Tease No longer!");
-    tease_mode = false;
-  }
+  tease_mode = false;
 }
+
+
+
+/**************************
+ * Switch Functions
+ **************************/
 
 void check_for_bothering()
 {
@@ -135,7 +170,7 @@ void check_for_bothering()
   unsigned long cur_timestamp = millis();
   
   // Check whether previous click was not too far ago
-  if(cur_timestamp - last_click_timestamp > fastClickThreshold)
+  if(cur_timestamp - last_click_timestamp > FastClickThreshold)
   {
     // If last click was to far back (normal user) - reset the clicks counter
     Serial.println("  Check Slow Click!");
@@ -151,55 +186,82 @@ void check_for_bothering()
   last_click_timestamp = cur_timestamp;
   
   // If user bother us with clicking - enter tease mode
-  if(fast_clicks > clicksForTeaseMode)
+  if(fast_clicks > ClicksForTeaseMode)
   {
     Serial.println("  Check Tease mode");
     tease_mode = true;
   }
 }
 
-bool should_tease()
-{
 
-  unsigned long cur_timestamp = millis();
+/**************************
+ * Servo Functions
+ **************************/
 
-  // Check whether there was no action during last few seconds so we may tease
-  if(tease_mode && (cur_timestamp - last_click_timestamp > teaseThreshold))
-    return true;
-    
-  return false;
+// Turn the switch off and add a little slam at the end to make sure
+bool turnOffSwitch(int step_delay) {
+    move_servo(ServoMaxPos - SlamDistance, step_delay, true);
+    turnSwitchOffServo.write(ServoMaxPos);   // Slam the switch off
+    delay(100);
 }
 
-void loop() 
+// Move servo to home position
+void servoHome(int step_delay) {
+  servoHome(step_delay, false);
+}
+void servoHome(int step_delay, bool look_for_switch) {
+  move_servo(ServoMinPos, step_delay, look_for_switch);
+}
+
+
+// Move the servo 
+bool move_servo(int end_pos, int step_delay, bool look_for_switch)
 {
-  // Get a new random speed range each loop for variability
-  int speedTurnOff = random(1, 3);
-  int speedReturn = random(2, 8);
+  int cur_pos = turnSwitchOffServo.read();
 
-  if(should_tease())
-    tease();
+  /*
+  Serial.print("    Move to ");
+  Serial.print(end_pos);
+  Serial.print(" from ");
+  Serial.print(cur_pos);
+  Serial.print(" with delay ");
+  Serial.print(step_delay);
+  Serial.print(" looking for switch ");
+  Serial.println(look_for_switch);
+  */
 
-  //TODO Sleep while not turned on
-  // Wait for click
-  if(digitalRead(switchPin)) {
-    
-    Serial.println("Switch turned ON");
-    Serial.println(digitalRead(switchPin));
-    
-    check_for_bothering();
-  
-    // Move forward until switch is off  
-    move_servo(servoMaxPos - slamDistance, speedTurnOff, true);
-    Serial.println(digitalRead(switchPin));
-    myservo.write(servoMaxPos);   // Slam the switch off
-    delay(200);
-    Serial.println(digitalRead(switchPin));
-
-    Serial.println("Switch turned OFF");
-    
-    // Now move backward until min pos or user clicked switch again
-    move_servo(servoMinPos, speedReturn, true);
-    Serial.println("Switch action END");
-    Serial.println();
+  // Check if we are already there
+  if(cur_pos == end_pos)
+  {
+    Serial.println("    Move Already there");
+    return true;
   }
-} 
+
+  // Move from current position to the end position  
+  int servo_step = cur_pos < end_pos ? 1 : -1;
+  int switching_state = (cur_pos >= end_pos);
+  //Serial.print("    Move step ");
+  //Serial.print(servo_step);
+  //Serial.print(" with switching state ");
+  //Serial.println(switching_state);
+  
+  for(; cur_pos != end_pos; cur_pos += servo_step)
+  {                                
+    turnSwitchOffServo.write(cur_pos);
+    delay(step_delay);
+
+    // If servo is moving forward - expect for switch off
+    // if moving backward - switch on will break the loop
+    if(look_for_switch && (digitalRead(SwitchPin) == switching_state))
+    {
+      //Serial.print("    Move Switch change detected to ");
+      //Serial.println(digitalRead(SwitchPin));
+      return false;
+    }
+  } 
+  
+  //Serial.println("    Move Done");
+  return true;
+}
+
+ 
